b4966eb05c387fda26894064184990f4b1ee69ef
nurfuis <16823188+nurfuis@users.noreply.github.com>
2023-09-10 16:37:40 -0500

    Add files via upload

--- battletower/js/animation.js
+++ battletower/js/animation.js
@@ -1,15 +1,14 @@
 // ANIMATION
 // textFillAnimation fills text into a container 1 character
-// at a time. It has an adjustable rate of fire and will stop 
+// at a time. It clickFastFillhas an adjustable rate of fire and will stop 
 // playing the sound and animation if the screens are changed.
+let clickArea = "main-menu-text-box";
 let fillText = "Welcome to the battle tower, where tough competitors await your challenge! Choose your character, select your power, and start battling. But be warned, there are strong opponents around every corner. Can you survive the melee and become a champion?";
 let textIsPlaying = false;
 
 function textFillAnimation(elementId, screenId) {
-	const animationBox = document.getElementById(elementId);
-	if (fillText === null) {
-		fillText = animationBox.textContent;		
-	}
+	const textElement = document.getElementById(elementId);
+
 	if (textIsPlaying) {
 		return;
 	}
@@ -18,14 +17,18 @@ function textFillAnimation(elementId, screenId) {
 	const screenDisplay = getComputedStyle(animationScreen) 
 	const characterLimit = 190;
 	var i = 0;
-	var delay = 5;
+	var delay = 50;
 
 	// Sound
 	const textFillSoundEffect = "effect7";
 	gameSound.playEffect(textFillSoundEffect);
 	
 	// Clear Text Box
-	animationBox.innerHTML = "";
+	textElement.innerHTML = "";
+	
+	// Fast fill
+	
+	textElement.addEventListener("click", () => { delay = 0 });
 	
 	// Fill Text
 	function addNextLetterToTextBox(){
@@ -34,13 +37,13 @@ function textFillAnimation(elementId, screenId) {
 				// Sound
 				gameSound.stopEffect(textFillSoundEffect);
 			}
-			if (animationBox.innerText.length >= characterLimit) {
-				animationBox.innerText = "-" + animationBox.innerText.substring(characterLimit / 5);
+			if (textElement.innerText.length >= characterLimit) {
+				textElement.innerText = "-" + textElement.innerText.substring(characterLimit / 5);
 			}	
-			animationBox.innerHTML += fillText[i];
+			textElement.innerHTML += fillText[i];
 			i+= 1;
 			// Get a random number between 100 and 300 milliseconds
-			delay = Math.floor(Math.random() * (70 - 35) + 36);
+			// delay = Math.floor(Math.random() * (70 - 35) + 36);
 			setTimeout(addNextLetterToTextBox, delay);
 			if (i === fillText.length) {
 				gameSound.stopEffect(textFillSoundEffect);
