befae21d761aad6ec434489790ba73ece8869fa1
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-01-04 10:40:10 -0600

    Delete brew/src directory

--- brew/src/objects/Foreground.js
+++
@@ -1,9 +0,0 @@
-// Foreground.js
-import { GameObject } from "../GameObject.js";
-import { Vector2 } from "../Vector2.js";
-export class Foreground extends GameObject {
-  constructor() {
-    super({});        
-  } // end of consctructor
-  // Methods
-}
