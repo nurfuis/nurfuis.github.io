c9b8786e4106464fddb0ea304941f842118e00d1
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-01-28 15:13:49 -0600

    Add files via upload

--- feyBrewster/src/Camera.js
+++ feyBrewster/src/Camera.js
@@ -25,12 +25,11 @@ export class Camera extends GameObject {
     this.homePosition = new Vector2(0,0);
     
     this.movementSpeed = 3;
-    this.currentScale = 1;
     this.isPanning = false;    
     
     this.screenShift = 'ready'; // ready |  complete | charging   
-    this.shiftTiming = 80; // frames
-    this.shiftDistance = 48; // pixels
+    this.shiftTiming = 0; // frames
+    this.shiftDistance = 64; // pixels
     this.shiftCounter = this.shiftDistance; 
      
     this.personHalf = gridSize / 2;  
@@ -48,34 +47,31 @@ export class Camera extends GameObject {
   }
   updateView(position) { 
 
-    const transformX = -position.x + this.halfWidth * (1 / this.currentScale);
-    const transformY = -position.y + this.halfHeight * (1 / this.currentScale);
+    const transformX = Math.round(-position.x + this.halfWidth);
+    const transformY = Math.round(-position.y + this.halfHeight);
+    
     const transform = new Vector2(transformX, transformY);
-    if (this.isPanning) {
-      this.homePosition = transform.duplicate();
-      this.driftTowards(transform);
-    } else {
-      this.position = transform;
-    }
+    
+    this.driftTowards(transform);
+    
+    this.homePosition = transform.duplicate();
+
   }
   driftTowards(center) {
-    if (this.isPanning) {
-      const driftAmount = 0.02;
+    const driftAmount = 0.02;
 
-      const roundedPosition = new Vector2(
-        Math.round(this.position.x + driftAmount * (center.x - this.position.x)),
-        Math.round(this.position.y + driftAmount * (center.y - this.position.y))
-      );
+    const roundedPosition = new Vector2(
+      Math.round(this.position.x + driftAmount * (center.x - this.position.x)),
+      Math.round(this.position.y + driftAmount * (center.y - this.position.y))
+    );
 
-      this.position = roundedPosition;
+    this.position = roundedPosition;
 
-      if (
-        Math.abs(this.position.x - center.x) <= 1 &&
-        Math.abs(this.position.y - center.y) <= 1
-      ) {
-        this.position = center.duplicate();
-        this.isPanning = false;
-      }
+    if (
+      Math.abs(this.position.x - center.x) <= 1 &&
+      Math.abs(this.position.y - center.y) <= 1
+    ) {
+      this.shiftCamera();
     }
   }
   shiftCamera() {
