2012c8e7f69c28fcc6386bcf3b091fceb6260990
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-01-27 22:45:13 -0600

    Add files via upload
    improved collision check to prevent wall clipping when objects are pushed by testing bounding volumes of both collidee and collider.

--- feyBrewster/src/helpers/grid.js
+++ feyBrewster/src/helpers/grid.js
@@ -7,34 +7,44 @@ export const gridCells = n => {
 export const obstacles = [];
 
 export const isSpaceFree = (posX, posY, collider) => {
-  const startTime = Date.now();
-  
   for (let i = 0; i < obstacles.length; i++) {
     const collidee = obstacles[i];
-    
-    const x1 = collidee.position.x;
-    const x2 = x1 + collidee.width;
-    
-    const y1 = collidee.position.y;
-    const y2 = y1 + collidee.height;
 
+    // Calculate bounding areas for both collider and collidee
+    const colliderX1 = posX;
+    const colliderX2 = colliderX1 + collider.width;
+    const colliderY1 = posY;
+    const colliderY2 = colliderY1 + collider.height;
+
+    const collideeX1 = collidee.position.x;
+    const collideeX2 = collideeX1 + collidee.width;
+    const collideeY1 = collidee.position.y;
+    const collideeY2 = collideeY1 + collidee.height;
+
+    if (collidee == collider) {
+      return { collisionDetected: false };
+    }
+
+    
     if (    
-      posX >= x1 && 
-      posX < x2 &&
-      posY >= y1 &&
-      posY < y2 &&
-      collidee != collider
-      ) {               
-      return { collisionDetected: true,
-               collider: collider,      
-               collidee: collidee
-      };
-    };
+      colliderX1 >= collideeX1 && 
+      colliderX1 < collideeX2 && 
+      colliderY1 >= collideeY1 &&
+      colliderY1 < collideeY2 
+      ) {
+      return { collisionDetected: true, collider: collider, collidee: collidee };
+    }
+    if (    
+      collideeX1 >= colliderX1 && 
+      collideeX1 < colliderX2 &&
+      collideeY1 >= colliderY1 &&
+      collideeY1 < colliderY2
+      ) {
+      return { collisionDetected: true, collider: collider, collidee: collidee };
+    }    
+    
+    
   }
-  
-  const endTime = Date.now();
-  const executionTime = endTime - startTime;
-  // console.log(`Execution time of isSpaceFree: ${executionTime.toFixed(3)} milliseconds`); 
-  
-  return { collisionDetected: false };   
-}
+
+  return { collisionDetected: false };
+};
