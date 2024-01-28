e1c030ef72ec809ce9678fb038e6a92618c9eeba
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-01-28 11:26:39 -0600

    Delete feyBrewster/src/levels/brewhouse directory

--- feyBrewster/src/levels/brewhouse/woodenfvTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="woodenfvTileset" tilewidth="32" tileheight="32" tilecount="12" columns="4">
- <image source="../../../public/sprites/woodenfvTileset.png" width="128" height="96"/>
-</tileset>
