afad11cfc651410b54eefadea6ee50fa38c7ead5
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-06-28 08:49:21 -0500

    1

---
+++ trailMap/components/nav.php
@@ -0,0 +1,6 @@
+<div class="nav__wrapper">
+  <a href="index.php">HOME</a> | <a href="trail-conditions.html">TRAILS</a> |
+  <a href="#">CAMPS</a> | <a href="topo-map.html">MAP</a> |
+  <a href="#">ABOUT</a> |
+  <a href="#">GIVE</a>
+</div>
---
+++ trailMap/components/welcome-header.php
@@ -0,0 +1,20 @@
+<div class="welcome-header__wrapper">
+  <h1>Preserve access to Big Sur Wilderness Areas</h1>
+
+  <p>
+    Trails are dissappearing unless something is done to prevent it from
+    happening. Please consider giving to help support trail work in the Big Sur
+    wild and to stop this treasure from being eroded, grown over, then lost.
+  </p>
+
+  <h2>Trail Information</h2>
+
+  <p>
+    Get up to date information about trail conditions or leave a report of your
+    own.
+  </p>
+
+  <h2>Camping Information</h2>
+
+  <p>Find information on roughing it in the Big Sur backcountry.</p>
+</div>
