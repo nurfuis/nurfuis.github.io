1cdb3626d4ccfbcdf4327a91aeea39431017dacf
nurfuis <16823188+nurfuis@users.noreply.github.com>
2024-07-01 12:37:12 -0500

    1

--- trailMap/components/nav.php
+++
@@ -1,6 +0,0 @@
-<div class="nav__wrapper">
-  <a href="./">HOME</a> | <a href="trail-conditions.html">TRAILS</a> |
-  <a href="#">CAMPS</a> | <a href="topo-map.html">MAP</a> |
-  <a href="#">ABOUT</a> |
-  <a href="#">GIVE</a>
-</div>
